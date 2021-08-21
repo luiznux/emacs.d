00263dfdd6afa5a116197be7ba5f0d04b8243044
luiznux <luiztagli@hotmail.com>
2021-08-21 02:46:30 -0300

    add the config

---
+++ lisp/code/clojure-config.el
@@ -0,0 +1,44 @@
+
+;;CLOJURE
+(use-package clojure-snippets)
+(use-package inf-clojure)
+(use-package zprint-mode)
+(use-package clojure-mode)
+(use-package clj-refactor
+  :config
+  (setq cljr-hotload-dependencies t))
+
+(use-package cider
+  :config
+  (setq cider-repl-pop-to-buffer-on-connect      'nil ;;display-only
+        cider-completion-annotations-include-ns  'always
+        cider-prompt-for-symbol                  t
+        nrepl-hide-special-buffers               t
+        cider-repl-use-content-types             t
+        cider-repl-wrap-history                  t
+        cider-repl-use-clojure-font-lock         t
+        cider-repl-buffer-size-limit             100000
+        cider-repl-history-size                  1000)
+
+  (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
+  (add-hook 'cider-test-report-mode 'jcf-soft-wrap)
+  (add-hook 'cider-repl-mode-hook #'cider-company-enable-fuzzy-completion)
+  (add-hook 'cider-mode-hook #'cider-company-enable-fuzzy-completion))
+
+(use-package eros
+  :config
+  (eros-mode 1))
+
+(use-package dizzee
+  :commands (jcf-lein-datomic-start jcf-lein-headless-start)
+  :config
+  (dz-defservice jcf-lein-headless
+                 "lein"
+                 :cd "~/"
+                 :args ("repl" ":headless"))
+
+  (dz-defservice jcf-lein-datomic
+                 "lein"
+                 :args ("datomic")))
+
+(provide 'clojure-config)
