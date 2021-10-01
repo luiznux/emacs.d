9e559deb36e783890039c73317d0ae5f992868c9
luiznux <luiztagli@hotmail.com>
2021-09-30 23:06:45 -0300

    update agenda color deffs

--- .emacs.d/lisp/org-config/file-color-agenda.el
+++ .emacs.d/lisp/org-config/file-color-agenda.el
@@ -3,23 +3,10 @@
 ;;; Emacs org agenda config
 ;;; Code:
 
-(defun ll/org/agenda/color-headers-with (tag col col2)
-  "Color agenda lines matching TAG with color COL(foreground) and COL2(background)."
-  (interactive)
-  (goto-char (point-min))
-  (while (re-search-forward tag nil t)
-                                        ;(Unless (find-in-line "\\[#[A-Z]\\]")
-    (let ((todo-end (or (ll/org/agenda/find-todo-word-end)
-                        (point-at-bol)))
-          (tags-beginning (or (find-in-line " :" t)
-                              (point-at-eol))))
-      (add-text-properties todo-end
-                           tags-beginning
-                           `(face (:foreground ,col :background ,col2 :weight bold))))))
 ;; Helper definitions
 (setq ll/org/agenda-todo-words
-      '("work :" "coll:" "project:" "agenda :" "bday :" "cap :"))
-
+      '("work  " "coll 🎓 " "project  " "agenda  " "habit 🍩 " "bday  " "cap  "
+        "Work Stuffs 🖥 " "College" "My Projects  "))
 
 (defun find-in-line (needle &optional beginning count)
   "Find the position of the start of NEEDLE in the current line.
@@ -35,24 +22,40 @@ from the left."
           (match-beginning 0)
         found))))
 
-
 (defun ll/org/agenda/find-todo-word-end ()
-
   "Find TODO keyword on the end."
 
   (reduce (lambda (a b) (or a b))
           (mapcar #'find-in-line ll/org/agenda-todo-words)))
 
 
+(defun ll/org/agenda/color-headers-with (tag col col2)
+  "Color agenda lines matching TAG with color COL(foreground) and COL2(background)."
+  (interactive)
+  (goto-char (point-min))
+  (while (re-search-forward tag nil t)
+                                        ;(Unless (find-in-line "\\[#[A-Z]\\]")
+    (let ((todo-end (or (ll/org/agenda/find-todo-word-end)
+                        (point-at-bol)))
+          (tags-beginning (or (find-in-line tag t)
+                              (point-at-eol))))
+      (add-text-properties todo-end
+                           tags-beginning
+                           `(face (:foreground ,col :background ,col2 :weight bold))))))
+
 (defun ll/org/colorize-headings ()
   "Color all headings with :pers: colors."
 
-  (ll/org/agenda/color-headers-with "work :" "#2d2d2d" "#FA74B2")
-  (ll/org/agenda/color-headers-with "coll:" "#2d2d2d" "#c792ea")
-  (ll/org/agenda/color-headers-with "project:" "#2d2d2d" "#839ce4")
-  (ll/org/agenda/color-headers-with "agenda :" "#2d2d2d" "#da8548")
-  (ll/org/agenda/color-headers-with "bday :" "#2d2d2d" "#89ddff")
-  (ll/org/agenda/color-headers-with "cap :" "#2d2d2d" "#c3e88d"))
+  (ll/org/agenda/color-headers-with "work  " "#2d2d2d" "#FA74B2")
+  (ll/org/agenda/color-headers-with "coll 🎓 " "#2d2d2d" "#c792ea")
+  (ll/org/agenda/color-headers-with "project  " "#2d2d2d" "#839ce4")
+  (ll/org/agenda/color-headers-with "agenda  " "#2d2d2d" "#da8548")
+  (ll/org/agenda/color-headers-with "habit 🍩 " "#2d2d2d" "#e06c75")
+  (ll/org/agenda/color-headers-with "bday  " "#2d2d2d" "#89ddff")
+  (ll/org/agenda/color-headers-with "cap  " "#2d2d2d" "#c3e88d")
+  (ll/org/agenda/color-headers-with "Work Stuffs 🖥 " "#2d2d2d" "#6d8dad")
+  (ll/org/agenda/color-headers-with "College " "#2d2d2d" "#c792ea")
+  (ll/org/agenda/color-headers-with "My Projects  " "#2d2d2d" "#839ce4"))
 
 
 (add-hook 'org-agenda-finalize-hook #'ll/org/colorize-headings)
