17f3ff5ec0f21bfa0cf90c3e426bdd71564bd6ff
luiznux <luiztagli@hotmail.com>
2021-12-12 21:50:15 -0300

    add cc package config

--- .emacs.d/lisp/code/lsp-config.el
+++ .emacs.d/lisp/code/lsp-config.el
@@ -476,6 +476,18 @@
           (append '("compile_commands.json" ".ccls")
                   projectile-project-root-files-top-down-recurring))))
 
+;; C/C++ Mode
+(use-package cc-mode
+  :ensure nil
+  :bind (:map c-mode-base-map
+         ("C-c c" . compile))
+  :hook (c-mode-common . (lambda () (c-set-style "stroustrup")))
+  :init (setq-default c-basic-offset 4)
+  :config
+  (use-package modern-cpp-font-lock
+    :diminish
+    :init (modern-c++-font-lock-global-mode t)))
+
 
 (provide 'lsp-config)
 ;;; lsp-config.el ends here
