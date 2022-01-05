d0351b8d0641557adae4fead68ec85a9df23190c
luiznux <luiztagli@hotmail.com>
2022-01-05 19:09:11 -0300

    add some features to centaur tabs

--- .emacs.d/lisp/interface/centaur-tabs-config.el
+++ .emacs.d/lisp/interface/centaur-tabs-config.el
@@ -17,10 +17,12 @@
 
 (use-package centaur-tabs
   :defines evil-normal-state-map
+
   :commands (centaur-tabs-group-by-projectile-project
              centaur-tabs-get-group-name
              centaur-tabs-headline-match
-             centaur-tabs-change-fonts)
+             centaur-tabs-change-fonts
+             centaur-tabs-enable-buffer-reordering)
 
   :hook ((dashboard-mode . centaur-tabs-local-mode)
          (term-mode . centaur-tabs-local-mode)
@@ -28,25 +30,29 @@
          (org-agenda-mode . centaur-tabs-local-mode)
          (helpful-mode . centaur-tabs-local-mode))
 
-  :bind(("C-<prior>" . centaur-tabs-backward)
-        ("C-<next>" . centaur-tabs-forward)
-        ("C-c t" . centaur-tabs-counsel-switch-group)
-        (:map evil-normal-state-map
-         ("g t" . centaur-tabs-forward)
-         ("g T" . centaur-tabs-backward)))
-
+  :bind (("C-<prior>" . centaur-tabs-backward)
+         ("C-<next>" . centaur-tabs-forward)
+         ("C-c t" . centaur-tabs-counsel-switch-group)
+         (:map evil-normal-state-map
+          ("g t" . centaur-tabs-forward)
+          ("g T" . centaur-tabs-backward)))
   :config
+  (centaur-tabs-mode t)
   (setq centaur-tabs-style                    "chamfer"
+
         centaur-tabs-height                   32
-        centaur-tabs-set-icons                t
         centaur-tabs-set-bar                  'under
         x-underline-at-descent-line           t
         centaur-tabs-set-modified-marker      t
-        centaur-tabs-show-navigation-buttons  t)
+        centaur-tabs-set-icons                t
+        centaur-tabs-show-new-tab-button      t
+        centaur-tabs-show-navigation-buttons  nil
+        centaur-tabs-show-count               nil
+        centaur-tabs-adjust-buffer-order      t)
 
   (centaur-tabs-headline-match)
   (centaur-tabs-group-by-projectile-project)
-  (centaur-tabs-mode t)
+  (centaur-tabs-enable-buffer-reordering)
   (centaur-tabs-change-fonts "Source Code Pro" 80)
 
   (defun centaur-tabs-buffer-groups ()
