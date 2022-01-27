aace15748e492b9773c851ca5bbe085e2c18c25b
luiznux <luiztagli@hotmail.com>
2022-01-26 22:31:30 -0300

    feat: add elpa directory variable

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -31,6 +31,12 @@
 
 ;;; Code:
 
+;; Define elpa packages dir
+(setq package-user-dir (locate-user-emacs-file
+                        (concat
+                         (file-name-as-directory "elpa")
+                         emacs-version)))
+
 ;; Defer garbage collection further back in the startup process
 (setq gc-cons-threshold most-positive-fixnum
       gc-cons-percentage 0.5)
