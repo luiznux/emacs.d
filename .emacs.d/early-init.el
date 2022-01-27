e79b86e68bdf7c349dcbad5a7ee0db104c92a54f
luiznux <luiztagli@hotmail.com>
2022-01-26 23:54:16 -0300

    update header

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -1,34 +1,14 @@
 ;;; early-init.el --- Early initialization. -*- lexical-binding: t -*-
-
-;; Copyright (C) 2019-2020 Vincent Zhang
-
-;; Author: Vincent Zhang <seagle0128@gmail.com>
-;; URL: https://github.com/seagle0128/.emacs.d
-
-;; This file is not part of GNU Emacs.
 ;;
-;; This program is free software; you can redistribute it and/or
-;; modify it under the terms of the GNU General Public License as
-;; published by the Free Software Foundation; either version 2, or
-;; (at your option) any later version.
+;; Author: Luiz Tagliaferro <luiz@luiznux.com>
+;; URL: https://luiznux.com
+;; This file is free software :)
 ;;
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-;; General Public License for more details.
-;;
-;; You should have received a copy of the GNU General Public License
-;; along with this program; see the file COPYING.  If not, write to
-;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
-;; Floor, Boston, MA 02110-1301, USA.
-;;
-
 ;;; Commentary:
 ;;
 ;; Emacs 27 introduces early-init.el, which is run before init.el,
 ;; before package and UI initialization happens.
 ;;
-
 ;;; Code:
 
 ;; Define elpa packages dir
@@ -57,5 +37,4 @@
   (push '(ns-transparent-titlebar . t) default-frame-alist))
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; early-init.el ends here
