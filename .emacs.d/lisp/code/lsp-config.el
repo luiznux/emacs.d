da5b7b9e1eafeb62f913e370e11735563334d3ae
luiznux <luiztagli@hotmail.com>
2022-01-26 23:56:40 -0300

    update file

--- .emacs.d/lisp/code/lsp-config.el
+++ .emacs.d/lisp/code/lsp-config.el
@@ -481,12 +481,20 @@
 (use-package ccls
   :defines projectile-project-root-files-top-down-recurring
   :hook ((c-mode c++-mode objc-mode cuda-mode) . (lambda () (require 'ccls)))
-  :init (setq ccls-executable "/usr/bin/ccls")
   :config
   (with-eval-after-load 'projectile
     (setq projectile-project-root-files-top-down-recurring
           (append '("compile_commands.json" ".ccls")
-                  projectile-project-root-files-top-down-recurring))))
+                  projectile-project-root-files-top-down-recurring)))
+  (with-no-warnings
+    ;; FIXME: fail to call ccls.xref
+    ;; @see https://github.com/emacs-lsp/emacs-ccls/issues/109
+    (cl-defmethod my-lsp-execute-command
+      ((_server (eql ccls)) (command (eql ccls.xref)) arguments)
+      (when-let ((xrefs (lsp--locations-to-xref-items
+                         (lsp--send-execute-command (symbol-name command) arguments))))
+        (xref--show-xrefs xrefs nil)))
+    (advice-add #'lsp-execute-command :override #'my-lsp-execute-command)))
 
 ;; C/C++ Mode
 (use-package cc-mode
