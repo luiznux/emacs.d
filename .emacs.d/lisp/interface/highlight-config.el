99cb3d1bd108ef9c2245145df23d2e2183bcd111
luiznux <luiztagli@hotmail.com>
2022-03-25 00:09:05 -0300

    fix: update matching paren out screen func

--- .emacs.d/lisp/interface/highlight-config.el
+++ .emacs.d/lisp/interface/highlight-config.el
@@ -20,12 +20,53 @@
 (use-package paren
   :ensure nil
   :hook (after-init . show-paren-mode)
-  :init (setq show-paren-when-point-inside-paren t
-              show-paren-when-point-in-periphery t)
+  :init
+  (setq show-paren-when-point-inside-paren t
+        show-paren-when-point-in-periphery t
+        show-paren-highlight-openparen     t
+        show-paren-style                   'paren)
+
   :config
   (with-no-warnings
-    ;; Display matching line for off-screen paren.
-    (defun display-line-overlay (pos str &optional face)
+    ;; we will call `blink-matching-open` ourselves...
+    (remove-hook 'post-self-insert-hook
+                 #'blink-paren-post-self-insert-function)
+    ;; this still needs to be set for `blink-matching-open` to work
+    (setq blink-matching-paren 'show)
+
+    (let ((ov nil)) ; keep track of the overlay
+      (advice-add
+       #'show-paren-function
+       :after
+       (defun show-paren--off-screen+ (&rest _args)
+         "Display matching line for off-screen paren."
+         (when (overlayp ov)
+           (delete-overlay ov))
+         ;; check if it's appropriate to show match info,
+         ;; see `blink-paren-post-self-insert-function'
+         (when (and (overlay-buffer show-paren--overlay)
+                    (not (or cursor-in-echo-area
+                             executing-kbd-macro
+                             noninteractive
+                             (minibufferp)
+                             this-command))
+                    (and (not (bobp))
+                         (memq (char-syntax (char-before)) '(?\) ?\$)))
+                    (= 1 (logand 1 (- (point)
+                                      (save-excursion
+                                        (forward-char -1)
+                                        (skip-syntax-backward "/\\")
+                                        (point))))))
+           ;; rebind `minibuffer-message' called by
+           ;; `blink-matching-open' to handle the overlay display
+           (cl-letf (((symbol-function #'minibuffer-message)
+                      (lambda (msg &rest args)
+                        (let ((msg (apply #'format-message msg args)))
+                          (setq ov (display-line-overlay+
+                                    (window-start) msg ))))))
+             (blink-matching-open))))))
+
+    (defun display-line-overlay+ (pos str &optional face)
       "Display line at POS as STR with FACE.
 
 FACE defaults to inheriting from default and highlight."
@@ -35,38 +76,11 @@ FACE defaults to inheriting from default and highlight."
                                 (line-end-position)))))
         (overlay-put ol 'display str)
         (overlay-put ol 'face
-                     (or face '(:inherit highlight)))
+                     (or face '(:inherit default :inherit highlight)))
         ol))
+    )
+  )
 
-    (defvar-local show-paren--off-screen-overlay nil)
-    (defun show-paren-off-screen (&rest _args)
-      "Display matching line for off-screen paren."
-      (when (overlayp show-paren--off-screen-overlay)
-        (delete-overlay show-paren--off-screen-overlay))
-      ;; Check if it's appropriate to show match info,
-      (when (and (overlay-buffer show-paren--overlay)
-                 (not (or cursor-in-echo-area
-                          executing-kbd-macro
-                          noninteractive
-                          (minibufferp)
-                          this-command))
-                 (and (not (bobp))
-                      (memq (char-syntax (char-before)) '(?\) ?\$)))
-                 (= 1 (logand 1 (- (point)
-                                   (save-excursion
-                                     (forward-char -1)
-                                     (skip-syntax-backward "/\\")
-                                     (point))))))
-        ;; Rebind `minibuffer-message' called by `blink-matching-open'
-        ;; to handle the overlay display.
-        (cl-letf (((symbol-function #'minibuffer-message)
-                   (lambda (msg &rest args)
-                     (let ((msg (apply #'format-message msg args)))
-                       (setq show-paren--off-screen-overlay
-                             (display-line-overlay
-                              (window-start) msg ))))))
-          (blink-matching-open))))
-    (advice-add #'show-paren-function :after #'show-paren-off-screen)))
 
 ;; Highlight indentions
 (use-package highlight-indent-guides
