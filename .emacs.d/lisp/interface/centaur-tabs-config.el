c0e2d6b0f45ebd23be5970a85b7e1c867dbe5ad4
luiznux <luiztagli@hotmail.com>
2022-04-13 19:56:26 -0300

    Refactor centaur-tabs-mode

--- .emacs.d/lisp/interface/centaur-tabs-config.el
+++ .emacs.d/lisp/interface/centaur-tabs-config.el
@@ -26,8 +26,13 @@
 
   :hook ((dashboard-mode . centaur-tabs-local-mode)
          (term-mode . centaur-tabs-local-mode)
+         (vterm-mode . centaur-tabs-local-mode)
          (calendar-mode . centaur-tabs-local-mode)
          (org-agenda-mode . centaur-tabs-local-mode)
+         (git-commit-mode . centaur-tabs-local-mode)
+         (minibuffer-mode . centaur-tabs-local-mode)
+         (org-capture-mode . centaur-tabs-local-mode)
+         (which-key-mode . centaur-tabs-mode)
          (helpful-mode . centaur-tabs-local-mode))
 
   :bind (("C-<prior>" . centaur-tabs-backward)
@@ -36,10 +41,9 @@
          (:map evil-normal-state-map
           ("g t" . centaur-tabs-forward)
           ("g T" . centaur-tabs-backward)))
-  :config
-  (centaur-tabs-mode t)
-  (setq centaur-tabs-style                    "chamfer"
 
+  :init
+  (setq centaur-tabs-style                    "chamfer"
         centaur-tabs-height                   32
         centaur-tabs-set-bar                  'under
         x-underline-at-descent-line           t
@@ -54,83 +58,48 @@
   (centaur-tabs-group-by-projectile-project)
   (centaur-tabs-enable-buffer-reordering)
   (centaur-tabs-change-fonts (face-attribute 'default :font) 70)
+  (centaur-tabs-mode t)
 
-  (defun centaur-tabs-buffer-groups ()
-    "`centaur-tabs-buffer-groups' control buffers' group rules.
- Group centaur-tabs with mode if buffer is derived
-from `eshell-mode' `emacs-lisp-mode' `dired-mode' `org-mode' `magit-mode'.
- All buffer name start with * will group to \"Emacs\".
- Other buffer group by `centaur-tabs-get-group-name' with project name."
-    (list
-     (cond
-      ((or (string-equal "*" (substring (buffer-name) 0 1))
-           (memq major-mode '(magit-process-mode
-                              magit-status-mode
-                              magit-diff-mode
-                              magit-log-mode
-                              magit-file-mode
-                              magit-blob-mode
-                              magit-blame-mode
-                              )))
-       "Emacs")
-      ((derived-mode-p 'prog-mode)
-       "Editing")
-      ((derived-mode-p 'emacs-lisp-mode)
-       "Elisp")
-      ((derived-mode-p 'dired-mode)
-       "Dired")
-      ((memq major-mode '(helpful-mode
-                          help-mode))
-       "Help")
-      ((memq major-mode '(org-mode
-                          org-agenda-clockreport-mode
-                          org-src-mode
-                          org-agenda-mode
-                          org-beamer-mode
-                          org-indent-mode
-                          org-bullets-mode
-                          org-cdlatex-mode
-                          org-agenda-log-mode
-                          diary-mode))
-       "OrgMode")
-      (t
-       (centaur-tabs-get-group-name (current-buffer))))))
-  (defun centaur-tabs-hide-tab (x)
-    "Do no to show buffer X in tabs."
-    (let ((name (format "%s" x)))
-      (or
-       ;; Current window is not dedicated window.
-       (window-dedicated-p (selected-window))
-
-       ;; Buffer name not match below blacklist.
-       (string-prefix-p "*epc" name)
-       (string-prefix-p "*helm" name)
-       (string-prefix-p "*Helm" name)
-       (string-prefix-p "*Compile-Log*" name)
-       (string-prefix-p "*lsp" name)
-       (string-prefix-p "*company" name)
-       (string-prefix-p "*Flycheck" name)
-       (string-prefix-p "*tramp" name)
-       (string-prefix-p " *Mini" name)
-       (string-prefix-p "*help" name)
-       (string-prefix-p "*Help" name)
-       (string-prefix-p "*Help*" name)
-       (string-prefix-p "*straight" name)
-       (string-prefix-p " *temp" name)
-       (string-prefix-p "*Ibuffer*" name)
-       (string-prefix-p "*mybuf" name)
-       (string-prefix-p "diary" name)
-       (string-prefix-p "*Org Agenda*" name)
-       (string-prefix-p "*dashboard*" name)
-       (string-prefix-p "*eldoc-posframe-buffer*" name)
-       (string-prefix-p "*flycheck-posframe-buffer*" name)
-       (string-prefix-p "*which-key*" name)
-       (string-prefix-p "*LSP Error List*" name)
-
-       ;; Is not magit buffer.
-       (and (string-prefix-p "magit" name)
-	        (not (file-name-extension name)))))))
-
-
+  ;;  (defun centaur-tabs-hide-tab (x)
+  ;;    "Do no to show buffer X in tabs."
+  ;;    (let ((name (format "%s" x)))
+  ;;      (or
+  ;;       ;; Current window is not dedicated window.
+  ;;       (window-dedicated-p (selected-window))
+  ;;
+  ;;       ;; Buffer name not match below blacklist.
+  ;;       (string-prefix-p "*epc" name)
+  ;;       (string-prefix-p "*helm" name)
+  ;;       (string-prefix-p "*Helm" name)
+  ;;       (string-prefix-p "*Compile-Log*" name)
+  ;;       (string-prefix-p "*lsp" name)
+  ;;       (string-prefix-p "*company" name)
+  ;;       (string-prefix-p "*Flycheck" name)
+  ;;       (string-prefix-p "*tramp" name)
+  ;;       (string-prefix-p " *Mini" name)
+  ;;       (string-prefix-p "*help" name)
+  ;;       (string-prefix-p "*Help" name)
+  ;;       (string-prefix-p "*Help*" name)
+  ;;       (string-prefix-p "*straight" name)
+  ;;       (string-prefix-p " *temp" name)
+  ;;       (string-prefix-p "*Ibuffer*" name)
+  ;;       (string-prefix-p "*mybuf" name)
+  ;;       (string-prefix-p "diary" name)
+  ;;       (string-prefix-p "*Org Agenda*" name)
+  ;;       (string-prefix-p "*dashboard*" name)
+  ;;       (string-prefix-p "*eldoc-posframe-buffer*" name)
+  ;;       (string-prefix-p "*flycheck-posframe-buffer*" name)
+  ;;       (string-prefix-p "*which-key*" name)
+  ;;       (string-prefix-p "*LSP Error List*" name)
+  ;;       (string-prefix-p "*vterm*" name)
+  ;;       (string-prefix-p "COMMIT_EDITMSG" name) ;; magit commit buffer
+  ;;       (string-prefix-p "*Org Agenda*" name)
+  ;;       (string-prefix-p "*Org Select*" name)
+  ;;       (string-prefix-p "*Capture*" name)
+  ;;
+  ;;       ;; Is not magit buffer.
+  ;;       (and (string-prefix-p "magit" name)
+  ;;	        (not (file-name-extension name)))))))
+  )
 (provide 'centaur-tabs-config)
 ;;; centaur-tabs-config.el ends here
