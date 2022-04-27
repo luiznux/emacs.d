9dd3328ce21cf6d11e78e6bc8854c1621d8bed90
luiznux <luiztagli@hotmail.com>
2022-04-27 17:01:44 -0300

    update ivy config

--- .emacs.d/lisp/interface/ivy-config.el
+++ .emacs.d/lisp/interface/ivy-config.el
@@ -19,8 +19,9 @@
 ;;
 ;;; Code:
 
+(require 'functions)
+
 (use-package counsel
-  :defines wiper-action-recenter
   :commands (ivy-immediate-done
              ivy-alt-done
              ivy-set-occur
@@ -83,7 +84,11 @@
           ("M-u" . ivy-scroll-down-command)
           ("M-<" . ivy-beginning-of-buffer)
           ("M->" . ivy-end-of-buffer)
-          ("C-w" . ivy-yank-word)))
+          ("C-w" . ivy-yank-word))
+
+         :map swiper-map
+         ("M-s" . swiper-isearch-toggle)
+         ("M-%" . swiper-query-replace))
 
   :hook ((after-init . ivy-mode)
          (ivy-mode . counsel-mode))
@@ -113,6 +118,12 @@
   (when (executable-find "fd")
     (setq counsel-fzf-cmd
           "fd --type f --hidden --follow --exclude .git --color never '%s'"))
+
+  ;; Be compatible with `gls'
+  (when (and sys/macp (executable-find "gls"))
+    (setq counsel-find-file-occur-use-find nil
+          counsel-find-file-occur-cmd
+          "gls -a | grep -i -E '%s' | tr '\\n' '\\0' | xargs -0 gls -d --group-directories-first"))
 
   ;; ignore files
   (setq counsel-find-file-ignore-regexp
@@ -282,156 +293,168 @@
       (interactive)
       (ivy-quit-and-run
         (counsel-fzf (or ivy-text "") default-directory)))
-    (bind-key "<C-return>" #'my-counsel-find-file-toggle-fzf counsel-find-file-map)))
+    (bind-key "<C-return>" #'my-counsel-find-file-toggle-fzf counsel-find-file-map)
 
-;; flx is used as the fuzzy-matching indexer backend for ivy.
-(use-package flx
-  :after ivy)
+    ;; More actions
+    (ivy-add-actions
+     #'swiper-isearch
+     '(("r" my-ivy-switch-to-counsel-rg "rg")
+       ("d" my-ivy-switch-to-rg-dwim "rg dwim")
+       ("s" my-ivy-switch-to-swiper "swiper")
+       ("a" my-ivy-switch-to-swiper-all "swiper all")))
 
-;; Enhance M-x
-(use-package amx
-  :init (setq amx-history-length 20))
+    (ivy-add-actions
+     #'swiper
+     '(("r" my-ivy-switch-to-counsel-rg "rg")
+       ("d" my-ivy-switch-to-rg-dwim "rg dwim")
+       ("s" my-ivy-switch-to-swiper-isearch "swiper isearch")
+       ("a" my-ivy-switch-to-swiper-all "swiper all")))
 
-(use-package ivy-xref
-  :init
-  (when (boundp 'xref-show-definitions-function)
-    (setq xref-show-definitions-function #'ivy-xref-show-defs))
-  (setq xref-show-xrefs-function #'ivy-xref-show-xrefs))
+    (ivy-add-actions
+     #'swiper-all
+     '(("g" my-ivy-switch-to-counsel-git-grep "git grep")
+       ("r" my-ivy-switch-to-counsel-rg "rg")
+       ("d" my-ivy-switch-to-rg-dwim "rg dwim")
+       ("s" my-swiper-toggle-swiper-isearch "swiper isearch")
+       ("S" my-ivy-switch-to-swiper "swiper")))
 
-(use-package prescient
-  :commands prescient-persist-mode
-  :init (prescient-persist-mode 1))
+    (ivy-add-actions
+     #'counsel-rg
+     '(("s" my-ivy-switch-to-swiper-isearch "swiper isearch")
+       ("S" my-ivy-switch-to-swiper "swiper")
+       ("a" my-ivy-switch-to-swiper-all "swiper all")
+       ("d" my-ivy-switch-to-rg-dwim "rg dwim")))
 
-(use-package ivy-prescient
-  :commands ivy-prescient-re-builder
-  :custom-face
-  (ivy-minibuffer-match-face-1 ((t (:foreground ,(face-foreground 'font-lock-doc-face nil t)))))
-  :init
-  (defun ivy-prescient-non-fuzzy (str)
-    "Generate an Ivy-formatted non-fuzzy regexp list for the given STR.
+    (ivy-add-actions
+     #'counsel-git-grep
+     '(("s" my-ivy-switch-to-swiper-isearch "swiper isearch")
+       ("S" my-ivy-switch-to-swiper "swiper")
+       ("r" my-ivy-switch-to-rg-dwim "rg")
+       ("d" my-ivy-switch-to-rg-dwim "rg dwim")
+       ("a" my-ivy-switch-to-swiper-all "swiper all")))
+
+    (ivy-add-actions
+     #'counsel-find-file
+     '(("g" my-ivy-switch-to-counsel-git "git")
+       ("z" my-ivy-switch-to-counsel-fzf "fzf")))
+
+    (ivy-add-actions
+     #'counsel-git
+     '(("f" my-ivy-switch-to-counsel-find-file "find file")
+       ("z" my-ivy-switch-to-counsel-fzf "fzf")))
+
+    (ivy-add-actions
+     'counsel-fzf
+     '(("f" my-ivy-switch-to-counsel-find-file "find file")
+       ("g" my-ivy-switch-to-counsel-git "git")))
+
+    (ivy-add-actions
+     'counsel-find-library
+     '(("p" my-ivy-copy-library-path "copy path")))
+    (ivy-add-actions
+     'counsel-load-library
+     '(("p" my-ivy-copy-library-path "copy path"))))
+
+  ;; Enhance M-x
+  (use-package amx
+    :init (setq amx-history-length 20))
+
+  (use-package prescient
+    :commands prescient-persist-mode
+    :init (prescient-persist-mode 1))
+
+  (use-package ivy-prescient
+    :commands ivy-prescient-re-builder
+    :custom-face
+    (ivy-minibuffer-match-face-1 ((t (:foreground ,(face-foreground 'font-lock-doc-face nil t)))))
+    :init
+    (defun ivy-prescient-non-fuzzy (str)
+      "Generate an Ivy-formatted non-fuzzy regexp list for the given STR.
 This is for use in `ivy-re-builders-alist'."
-    (let ((prescient-filter-method '(literal regexp)))
-      (ivy-prescient-re-builder str)))
+      (let ((prescient-filter-method '(literal regexp)))
+        (ivy-prescient-re-builder str)))
 
-  (setq ivy-prescient-retain-classic-highlighting t
-        ivy-re-builders-alist
-        '((counsel-ag . ivy-prescient-non-fuzzy)
-          (counsel-rg . ivy-prescient-non-fuzzy)
-          (counsel-pt . ivy-prescient-non-fuzzy)
-          (counsel-grep . ivy-prescient-non-fuzzy)
-          (counsel-fzf . ivy-prescient-non-fuzzy)
-          (counsel-imenu . ivy-prescient-non-fuzzy)
-          (counsel-yank-pop . ivy-prescient-non-fuzzy)
-          (swiper . ivy-prescient-non-fuzzy)
-          (swiper-isearch . ivy-prescient-non-fuzzy)
-          (swiper-all . ivy-prescient-non-fuzzy)
-          (lsp-ivy-workspace-symbol . ivy-prescient-non-fuzzy)
-          (lsp-ivy-global-workspace-symbol . ivy-prescient-non-fuzzy)
-          (insert-char . ivy-prescient-non-fuzzy)
-          (counsel-unicode-char . ivy-prescient-non-fuzzy)
-          (t . ivy-prescient-re-builder))
-        ivy-prescient-sort-commands
-        '(counsel-M-x execute-extended-command execute-extended-command-for-buffer))
+    (setq ivy-prescient-retain-classic-highlighting t
+          ivy-re-builders-alist
+          '((counsel-ag . ivy-prescient-non-fuzzy)
+            (counsel-rg . ivy-prescient-non-fuzzy)
+            (counsel-pt . ivy-prescient-non-fuzzy)
+            (counsel-grep . ivy-prescient-non-fuzzy)
+            (counsel-fzf . ivy-prescient-non-fuzzy)
+            (counsel-imenu . ivy-prescient-non-fuzzy)
+            (counsel-yank-pop . ivy-prescient-non-fuzzy)
+            (swiper . ivy-prescient-non-fuzzy)
+            (swiper-isearch . ivy-prescient-non-fuzzy)
+            (swiper-all . ivy-prescient-non-fuzzy)
+            (lsp-ivy-workspace-symbol . ivy-prescient-non-fuzzy)
+            (lsp-ivy-global-workspace-symbol . ivy-prescient-non-fuzzy)
+            (insert-char . ivy-prescient-non-fuzzy)
+            (counsel-unicode-char . ivy-prescient-non-fuzzy)
+            (t . ivy-prescient-re-builder))
+          ivy-prescient-sort-commands
+          '(counsel-M-x execute-extended-command execute-extended-command-for-buffer))
 
-  (ivy-prescient-mode 1))
+    (ivy-prescient-mode 1))
 
-;; Ivy integration for Projectile
-(use-package counsel-projectile
-  :hook (counsel-mode . counsel-projectile-mode)
-  :init (setq counsel-projectile-grep-initial-input '(ivy-thing-at-point)))
+  ;; Ivy integration for Projectile
+  (use-package counsel-projectile
+    :hook (counsel-mode . counsel-projectile-mode)
+    :init (setq counsel-projectile-grep-initial-input '(ivy-thing-at-point)))
 
-;; Integrate yasnippet
-(use-package ivy-yasnippet
-  :bind ("C-c C-y" . ivy-yasnippet))
+  ;; Integrate yasnippet
+  (use-package ivy-yasnippet
+    :bind ("C-c C-y" . ivy-yasnippet))
 
-(use-package all-the-icons-ivy
-  :init (add-hook 'after-init-hook 'all-the-icons-ivy-setup)
-  :config
-  (setq all-the-icons-ivy-file-commands
-        '(counsel-find-file counsel-file-jump counsel-recentf counsel-projectile-find-file counsel-projectile-find-dir)))
+  ;; Quick launch apps
+  (cond
+   (sys/linux-x-p
+    (bind-key "s-<f6>" #'counsel-linux-app counsel-mode-map))
+   (sys/macp
+    (use-package counsel-osx-app
+      :bind (:map counsel-mode-map
+             ("s-<f6>" . counsel-osx-app)))))
 
-;; Ivy
-(use-package ivy-dired-history
-  :demand t
-  :after (savehist dired)
-  :bind (:map dired-mode-map
-         ("," . dired))
-  :init (add-to-list 'savehist-additional-variables 'ivy-dired-history-variable))
+  ;; Tramp ivy interface
+  (use-package counsel-tramp
+    :bind (:map counsel-mode-map
+           ("C-c c T" . counsel-tramp)))
 
-;; Better experience with icons
-;; Enable it before`ivy-rich-mode' for better performance
-(use-package all-the-icons-ivy-rich
-  :hook (ivy-mode . all-the-icons-ivy-rich-mode)
-  :config
-  (all-the-icons-ivy-rich-reload))
+  ;; Ivy
+  (use-package ivy-dired-history
+    :demand t
+    :after (savehist dired)
+    :bind (:map dired-mode-map
+           ("," . dired))
+    :init (add-to-list 'savehist-additional-variables 'ivy-dired-history-variable))
 
-;; More friendly display transformer for Ivy
-(use-package ivy-rich
-  :hook ((counsel-projectile-mode . ivy-rich-mode) ; MUST after `counsel-projectile'
-         (ivy-rich-mode . ivy-rich-project-root-cache-mode)
-         (ivy-rich-mode . (lambda ()
-                            "Use abbreviate in `ivy-rich-mode'."
-                            (setq ivy-virtual-abbreviate
-                                  (or (and ivy-rich-mode 'abbreviate) 'name)))))
-  :init
-  ;; For better performance
-  (setq ivy-rich-parse-remote-buffer nil))
+  ;; Better experience with icons
+  ;; Enable it before`ivy-rich-mode' for better performance
+  (use-package all-the-icons-ivy-rich
+    :hook (ivy-mode . all-the-icons-ivy-rich-mode)
+    :config
+    (plist-put all-the-icons-ivy-rich-display-transformers-list
+               'load-theme
+               '(:columns
+                 ((all-the-icons-ivy-rich-theme-icon)
+                  (ivy-rich-candidate))
+                 :delimiter "\t"))
+    (all-the-icons-ivy-rich-reload))
 
-;;(use-package ivy-rich
-;;  :commands ivy-format-function-line
-;;  :custom
-;;  (sivy-rich-path-style 'abbrev)
-;;  (ivy-rich-switch-buffer-align-virtual-buffer nil)
-;;  (ivy-rich-project-root-cache-mode t)
-;;  :init
-;;  (setq ivy-rich-parse-remote-buffer nil)
-;;  :config
-;;  (defun ivy-rich-switch-buffer-icon (candidate)
-;;    (with-current-buffer
-;;    (get-buffer candidate)
-;;                                                        (let ((icon (all-the-icons-icon-for-mode major-mode)))
-;;        (if (symbolp icon)
-;;    (all-the-icons-icon-for-mode 'fundamental-mode)
-;;                                                            icon))))
-;;
-;;                                                    (setq ivy-rich-display-transformers-list
-;;        '(ivy-switch-buffer
-;;          (:columns
-;;           ((ivy-rich-switch-buffer-icon (:width 2))
-;;            (ivy-rich-candidate (:width 30))
-;;            (ivy-rich-switch-buffer-size (:width 7))
-;;            (ivy-rich-switch-buffer-indicators (:width 4 :face error :align right))
-;;            (ivy-rich-switch-buffer-major-mode (:width 12 :face warning))
-;;            (ivy-rich-switch-buffer-project (:width 15 :face success))
-;;            (ivy-rich-switch-buffer-path (:width (lambda (x) (ivy-rich-switch-buffer-shorten-path x (ivy-rich-minibuffer-width 0.3))))))
-;;           :predicate
-;;           (lambda (cand) (get-buffer cand)))))
-;;
-;;                                                    (defcustom ivy-filthy-rich-padding ?\s
-;;                                                      "The padding of `ivy-filthy-rich-delimiter'.
-;;It is used when there are extra space.
-;;The length of the pad has to be one.
-;;If not, `ivy-filth-rich' will fallback to using space to pad.
-;;Currently only support character, because `make-string' only accept that."
-;;                                                      :type 'character
-;;                                                      :group 'ivy-filthy-rich)
-;;
-;;                                                    (defcustom ivy-filthy-rich-pad-side 'right
-;;                                                      "The side which padding is pad to.
-;;Either left or right.
-;;Left means align right,
-;;right means align left."
-;;                                                      :type 'symbol
-;;                                                      :group 'ivy-filthy-rich)
-;;
-;;                                                    (defcustom ivy-filthy-rich-max-length 0
-;;                                                      "The max length of one entry (one line on ivy buffer).
-;;If it is zero, the max-length is (1- (frame-width))"
-;;                                                      :type 'number
-;;                                                      :group 'ivy-filthy-rich)
-;;                                                    (setcdr (assq t ivy-format-functions-alist) #'ivy-format-function-line)
-;;                                                    (ivy-rich-mode))
+  ;; More friendly display transformer for Ivy
+  (use-package ivy-rich
+    :hook ((counsel-projectile-mode . ivy-rich-mode) ; MUST after `counsel-projectile'
+           (ivy-rich-mode . ivy-rich-project-root-cache-mode)
+           (ivy-rich-mode . (lambda ()
+                              "Use abbreviate in `ivy-rich-mode'."
+                              (setq ivy-virtual-abbreviate
+                                    (or (and ivy-rich-mode 'abbreviate) 'name)))))
+    :init
+    ;; For better performance
+    (setq ivy-rich-parse-remote-buffer nil))
 
+  ;; flx is used as the fuzzy-matching indexer backend for ivy.
+  (use-package flx
+    :after ivy))
 
 
 
