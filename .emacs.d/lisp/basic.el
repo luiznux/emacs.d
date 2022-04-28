6dc455b38c82415d5360db6681827574898a9b11
luiznux <luiztagli@hotmail.com>
2022-04-27 21:46:00 -0300

    add simple and some misc conf

--- .emacs.d/lisp/basic.el
+++ .emacs.d/lisp/basic.el
@@ -90,6 +90,120 @@
                                               extended-command-history)
               savehist-autosave-interval 300))
 
+(use-package simple
+  :ensure nil
+  :hook ((after-init . size-indication-mode)
+         (text-mode . visual-line-mode)
+         ((prog-mode markdown-mode conf-mode) . enable-trailing-whitespace))
+  :init
+  (setq column-number-mode t
+        line-number-mode t
+        ;; kill-whole-line t               ; Kill line including '\n'
+        line-move-visual nil
+        track-eol t                     ; Keep cursor at end of lines. Require line-move-visual is nil.
+        set-mark-command-repeat-pop t)  ; Repeating C-SPC after popping mark pops it again
+
+  ;; Visualize TAB, (HARD) SPACE, NEWLINE
+  (setq-default show-trailing-whitespace nil) ; Don't show trailing whitespace by default
+  (defun enable-trailing-whitespace ()
+    "Show trailing spaces and delete on saving."
+    (setq show-trailing-whitespace t)
+    (add-hook 'before-save-hook #'delete-trailing-whitespace nil t))
+
+  ;; Prettify the process list
+  (with-no-warnings
+    (define-derived-mode process-menu-mode tabulated-list-mode "Process Menu"
+      "Major mode for listing the processes called by Emacs."
+      (setq tabulated-list-format `[("" ,2 0)
+                                    ("Process" 25 t)
+			                        ("PID"      7 t)
+			                        ("Status"   7 t)
+                                    ;; 25 is the length of the long standard buffer
+                                    ;; name "*Async Shell Command*<10>" (bug#30016)
+			                        ("Buffer"  25 t)
+			                        ("TTY"     12 t)
+			                        ("Thread"  12 t)
+			                        ("Command"  0 t)])
+      (make-local-variable 'process-menu-query-only)
+      (setq tabulated-list-sort-key (cons "Process" nil))
+      (add-hook 'tabulated-list-revert-hook 'list-processes--refresh nil t))
+
+    (defun list-processes--refresh ()
+      "Recompute the list of processes for the Process List buffer.
+Also, delete any process that is exited or signaled."
+      (setq tabulated-list-entries nil)
+      (dolist (p (process-list))
+        (cond ((memq (process-status p) '(exit signal closed))
+	           (delete-process p))
+	          ((or (not process-menu-query-only)
+	               (process-query-on-exit-flag p))
+	           (let* ((icon
+                       (or (all-the-icons-octicon "zap"
+                                                    :height 1.0 :v-adjust -0.05
+                                                    :face 'all-the-icons-lblue)
+                        ""))
+                      (buf (process-buffer p))
+		              (type (process-type p))
+		              (pid  (if (process-id p) (format "%d" (process-id p)) "--"))
+		              (name (process-name p))
+                      (status (process-status p))
+		              (status `(,(symbol-name status)
+                                face ,(if (memq status '(stop exit closed failed))
+                                          'error
+                                        'success)))
+		              (buf-label (if (buffer-live-p buf)
+				                     `(,(buffer-name buf)
+				                       face link
+				                       help-echo ,(format-message
+					                               "Visit buffer `%s'"
+					                               (buffer-name buf))
+				                       follow-link t
+				                       process-buffer ,buf
+				                       action process-menu-visit-buffer)
+			                       "--"))
+		              (tty `(,(or (process-tty-name p) "--")
+                             face font-lock-doc-face))
+		              (thread
+                       `(,(cond
+                           ((or
+                             (null (process-thread p))
+                             (not (fboundp 'thread-name))) "--")
+                           ((eq (process-thread p) main-thread) "Main")
+		                   ((thread-name (process-thread p)))
+		                   (t "--"))
+                         face font-lock-doc-face))
+		              (cmd
+		               `(,(if (memq type '(network serial pipe))
+		                      (let ((contact (process-contact p t t)))
+			                    (if (eq type 'network)
+			                        (format "(%s %s)"
+				                            (if (plist-get contact :type)
+					                            "datagram"
+				                              "network")
+				                            (if (plist-get contact :server)
+					                            (format
+                                                 "server on %s"
+					                             (if (plist-get contact :host)
+                                                     (format "%s:%s"
+						                                     (plist-get contact :host)
+                                                             (plist-get
+                                                              contact :service))
+					                               (plist-get contact :local)))
+				                              (format "connection to %s:%s"
+					                                  (plist-get contact :host)
+					                                  (plist-get contact :service))))
+			                      (format "(serial port %s%s)"
+				                          (or (plist-get contact :port) "?")
+				                          (let ((speed (plist-get contact :speed)))
+				                            (if speed
+					                            (format " at %s b/s" speed)
+				                              "")))))
+		                    (mapconcat 'identity (process-command p) " "))
+                         face completions-annotations)))
+	             (push (list p (vector icon name pid status buf-label tty thread cmd))
+		               tabulated-list-entries)))))
+      (tabulated-list-init-header))))
+
 (when emacs/>=27p
   (use-package so-long
     :ensure nil
@@ -108,13 +222,14 @@
   (fset 'yes-or-no-p 'y-or-n-p))
 
 (setq indent-line-function 'insert-tab)
-(setq-default indent-tabs-mode nil ; Permanently indent with spaces, never with TABs
-              tab-width        4)
+(setq-default major-mode        'text-mode
+              fill-column       80
+              tab-width         4
+              indent-tabs-mode  nil) ; Permanently indent with spaces, never with TABs
 
-(setq password-cache-expiry      nil
-      load-prefer-newer          t
-      system-time-locale         "C")
-
+(setq password-cache-expiry     nil
+      load-prefer-newer         t
+      system-time-locale        "C")
 
 (setq line-breaker page-delimiter)
 
