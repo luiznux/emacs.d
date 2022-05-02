b9008797390baddff74afd3ae8c5c785e41c8e01
luiznux <luiztagli@hotmail.com>
2022-05-01 22:23:37 -0300

    refactor evil config

--- .emacs.d/lisp/code/evil-config.el
+++ .emacs.d/lisp/code/evil-config.el
@@ -18,52 +18,71 @@
 ;;; Code:
 
 (use-package evil
+  :demand t
   :commands evil-set-initial-state
   :init
   (setq evil-want-integration t
         evil-want-keybinding nil
         evil-undo-system 'undo-tree)
-  :config
-  (evil-mode 1)
-  (evil-set-initial-state 'term-mode 'emacs)
+
   (with-eval-after-load 'evil
     (define-key evil-normal-state-map (kbd "M-.") nil)
-    (define-key evil-normal-state-map (kbd "C-u") 'evil-scroll-up)))
+    (define-key evil-normal-state-map (kbd "C-u") 'evil-scroll-up))
 
-(use-package evil-collection
-  :after evil
-  :defines evil-collection-company-use-tng
-  :init
-  (setq evil-collection-setup-minibuffer nil
-        evil-collection-company-use-tng  nil)
   :config
-  (evil-collection-init))
+  (evil-mode 1)
 
-(use-package evil-matchit
-  :config
-  (global-evil-matchit-mode 1))
+  ;; dependency for evil-undo-system
+  (use-package undo-tree
+    :hook (after-init . global-undo-tree-mode))
 
-(use-package evil-surround
-  :config
-  (global-evil-surround-mode 1))
+  (use-package evil-collection
+    :demand t
+    :after evil
+    :defines evil-collection-company-use-tng
+    :init
+    (setq evil-collection-setup-minibuffer nil
+          evil-collection-company-use-tng  nil)
+    :config
+    (evil-collection-init))
 
-(use-package evil-leader
-  :config
-  (global-evil-leader-mode))
+  (use-package evil-org
+    :demand t
+    :functions evil-org-agenda-set-keys
+    :after evil org
+    :config
+    (add-hook 'org-mode-hook (lambda () (evil-org-mode)))
+    (require 'evil-org-agenda)
+    (evil-org-agenda-set-keys)
+    (with-no-warnings
+      (evil-org-set-key-theme '(textobjects insert navigation additional shift todo heading))))
 
-(use-package evil-org
-  :functions evil-org-agenda-set-keys
-  :after evil org
-  :config
-  (add-hook 'org-mode-hook (lambda () (evil-org-mode)))
-  (require 'evil-org-agenda)
-  (evil-org-agenda-set-keys)
-  (with-no-warnings
-    (evil-org-set-key-theme '(textobjects insert navigation additional shift todo heading))))
+  (use-package evil-multiedit
+    :config
+    (with-no-warnings
+      (evil-multiedit-default-keybinds)))
 
-;; dependency for evil-undo-system
-(use-package undo-tree
-  :hook (after-init . global-undo-tree-mode))
+  (use-package evil-goggles
+    :init
+    (custom-set-faces
+     '(evil-goggles-delete-face ((t (:inherit 'shadow))))
+     '(evil-goggles-paste-face ((t (:inherit 'lazy-highlight))))
+     '(evil-goggles-yank-face ((t (:inherit 'isearch-fail)))))
+
+    (setq evil-goggles-mode t)
+    )
+
+  (use-package evil-surround
+    :config
+    (global-evil-surround-mode 1))
+
+  (use-package evil-leader
+    :config
+    (global-evil-leader-mode))
+
+  (use-package evil-matchit
+    :config
+    (global-evil-matchit-mode 1)))
 
 
 (provide 'evil-config)
