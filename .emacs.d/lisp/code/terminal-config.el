2fc285878ec847b40e163d5c5eca74b5274b9c0f
luiznux <luiztagli@hotmail.com>
2022-05-05 15:52:19 -0300

    refactor terminal config

--- .emacs.d/lisp/code/terminal-config.el
+++ .emacs.d/lisp/code/terminal-config.el
@@ -17,6 +17,79 @@
 ;;
 ;;; Code:
 
+(use-package shell
+  :ensure nil
+  :hook ((shell-mode . my-shell-mode-hook)
+         (comint-output-filter-functions . comint-strip-ctrl-m))
+  :init
+  (setq system-uses-terminfo nil)
+
+  (with-no-warnings
+    (defun my-shell-simple-send (proc command)
+      "Various PROC COMMANDs pre-processing before sending to shell."
+      (cond
+       ;; Checking for clear command and execute it.
+       ((string-match "^[ \t]*clear[ \t]*$" command)
+        (comint-send-string proc "\n")
+        (erase-buffer))
+       ;; Checking for man command and execute it.
+       ((string-match "^[ \t]*man[ \t]*" command)
+        (comint-send-string proc "\n")
+        (setq command (replace-regexp-in-string "^[ \t]*man[ \t]*" "" command))
+        (setq command (replace-regexp-in-string "[ \t]+$" "" command))
+        ;;(message (format "command %s command" command))
+        (funcall 'man command))
+       ;; Send other commands to the default handler.
+       (t (comint-simple-send proc command))))
+
+    (defun my-shell-mode-hook ()
+      "Shell mode customization."
+      (local-set-key '[up] 'comint-previous-input)
+      (local-set-key '[down] 'comint-next-input)
+      (local-set-key '[(shift tab)] 'comint-next-matching-input-from-input)
+
+      (ansi-color-for-comint-mode-on)
+      (setq comint-input-sender 'my-shell-simple-send))))
+
+;; ANSI & XTERM 256 color support
+(use-package xterm-color
+  :defines (compilation-environment
+            eshell-preoutput-filter-functions
+            eshell-output-filter-functions)
+  :functions (compilation-filter my-advice-compilation-filter)
+  :init
+  ;; For shell and interpreters
+  (setenv "TERM" "xterm-256color")
+  (setq comint-output-filter-functions
+        (remove 'ansi-color-process-output comint-output-filter-functions))
+  (add-hook 'comint-preoutput-filter-functions 'xterm-color-filter)
+  (add-hook 'shell-mode-hook
+            (lambda ()
+              ;; Disable font-locking to improve performance
+              (font-lock-mode -1)
+              ;; Prevent font-locking from being re-enabled
+              (make-local-variable 'font-lock-function)
+              (setq font-lock-function #'ignore)))
+
+  ;; For eshell
+  (with-eval-after-load 'esh-mode
+    (add-hook 'eshell-before-prompt-hook
+              (lambda ()
+                (setq xterm-color-preserve-properties t)))
+    (add-to-list 'eshell-preoutput-filter-functions 'xterm-color-filter)
+    (setq eshell-output-filter-functions
+          (remove 'eshell-handle-ansi-color eshell-output-filter-functions)))
+
+  ;; For compilation buffers
+  (setq compilation-environment '("TERM=xterm-256color"))
+  (defun my-advice-compilation-filter (f proc string)
+    (funcall f proc
+             (if (eq major-mode 'rg-mode) ; compatible with `rg'
+                 string
+               (xterm-color-filter string))))
+  (advice-add 'compilation-filter :around #'my-advice-compilation-filter)
+  (advice-add 'gud-filter :around #'my-advice-compilation-filter))
+
 ;; Better terminal emulator
 ;; @see https://github.com/akermu/emacs-libvterm#installation
 (when (and module-file-suffix           ; dynamic module
@@ -34,19 +107,43 @@
 ;; Shell Pop: leverage `popper'
 (with-no-warnings
   (defvar shell-pop--frame nil)
-  (defun shell-pop-posframe-hidehandler (_)
-    "Hidehandler used by `shell-pop-posframe-toggle'."
-    (not (eq (selected-frame) posframe--frame)))
+  (defvar shell-pop--window nil)
 
   (defun shell-pop--shell (&optional arg)
-    "Get shell buffer."
+    "Run shell and return the buffer."
     (cond ((fboundp 'vterm) (vterm arg))
           (sys/win32p (eshell arg))
           (t (shell))))
 
+  (defun shell-pop--hide-frame ()
+    "Hide child frame and refocus in parent frame."
+    (when (and (frame-live-p shell-pop--frame)
+               (frame-visible-p shell-pop--frame))
+      (make-frame-invisible shell-pop--frame)
+      (select-frame-set-input-focus (frame-parent shell-pop--frame))
+      (setq shell-pop--frame nil)))
+
+  (defun shell-pop-toggle ()
+    "Toggle shell."
+    (interactive)
+    (shell-pop--hide-frame)
+    (if (window-live-p shell-pop--window)
+        (progn
+          (delete-window shell-pop--window)
+          (setq shell-pop--window nil))
+      (setq shell-pop--window
+            (get-buffer-window (shell-pop--shell)))))
+  (bind-keys ([f9]  . shell-pop-toggle)
+             ("C-`" . shell-pop-toggle))
+
+  (defun shell-pop-posframe-hidehandler (_)
+    "Hidehandler used by `shell-pop-posframe-toggle'."
+    (not (eq (selected-frame) posframe--frame)))
+
   (defun shell-pop-posframe-toggle ()
     "Toggle shell in child frame."
     (interactive)
+
     (let* ((buffer (shell-pop--shell))
            (window (get-buffer-window buffer)))
       ;; Hide window: for `popper'
@@ -75,7 +172,7 @@
                  :min-width width
                  :min-height height
                  :internal-border-width 3
-                 :internal-border-color (face-foreground 'font-lock-comment-face nil t)
+                 :internal-border-color (face-background 'posframe-border nil t)
                  :background-color (face-background 'tooltip nil t)
                  :override-parameters '((cursor-type . t))
                  :accept-focus t))
@@ -88,21 +185,7 @@
             (goto-char (point-max))
             (when (fboundp 'vterm-reset-cursor-point)
               (vterm-reset-cursor-point)))))))
-  (bind-key "C-`" #'shell-pop-posframe-toggle)
-
-  (defvar shell-pop--window nil)
-  (defun shell-pop-toggle ()
-    "Toggle shell."
-    (interactive)
-    (unless (and (frame-live-p shell-pop--frame)
-                 (frame-visible-p shell-pop--frame))
-      (if (window-live-p shell-pop--window)
-          (progn
-            (delete-window shell-pop--window)
-            (setq shell-pop--window nil))
-        (setq shell-pop--window
-              (get-buffer-window (shell-pop--shell))))))
-  (bind-key [f9] #'shell-pop-toggle))
+  (bind-key "C-`" #'shell-pop-posframe-toggle))
 
 
 (provide 'terminal-config)
