ffd6273d1b6d04eb69514493ab6685bae9a2a611
luiznux <luiztagli@hotmail.com>
2022-06-20 20:50:09 -0300

    [feature] - add persp-config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -77,6 +77,7 @@
 (require 'window-config)
 (require 'ibuffer-config)
 (require 'highlight-config)
+(require 'persp-config)
 (require 'evil-config)
 (require 'company-config)
 (require 'bookmark-config)
