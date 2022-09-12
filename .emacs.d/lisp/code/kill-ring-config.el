6660411ce6c0703f207be9e86a4e13490d036164
luiznux <luiztagli@hotmail.com>
2022-09-11 23:03:06 -0300

    Enhance browse-kill-ring.

--- .emacs.d/lisp/code/kill-ring-config.el
+++ .emacs.d/lisp/code/kill-ring-config.el
@@ -28,8 +28,9 @@
 ;; Interactively insert and edit items from kill-ring
 (use-package browse-kill-ring
   :bind ("C-c k" . browse-kill-ring)
-  :hook (after-init . browse-kill-ring-default-keybindings))
-
+  :hook (after-init . browse-kill-ring-default-keybindings)
+  :init (setq browse-kill-ring-separator "────────────────"
+              browse-kill-ring-separator-face 'shadow))
 
 (provide 'kill-ring-config)
 ;;; kill-ring-config.el ends here
