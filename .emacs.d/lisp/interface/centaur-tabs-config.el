aebfef3516528b3c6ffa50efb9559e4a501dfd98
luiznux <luiztagli@hotmail.com>
2023-02-04 18:15:32 -0300

    [Refactor] - Centaur tabs
    1. Format code
    2. Use after-init-hook instead of just calling "cantaur-tabs-mode".
    3. add " *" to excluded-prefixes-append-list to avoid displaying centaur tabs in
        any buffer that stars with it.

--- .emacs.d/lisp/interface/centaur-tabs-config.el
+++ .emacs.d/lisp/interface/centaur-tabs-config.el
@@ -20,30 +20,27 @@
 
 (use-package centaur-tabs
   :defines (evil-normal-state-map centaur-tabs-excluded-prefixes)
-
   :commands (centaur-tabs-group-by-projectile-project
              centaur-tabs-get-group-name
              centaur-tabs-headline-match
              centaur-tabs-change-fonts
              centaur-tabs-enable-buffer-reordering)
-
-  :hook ((dashboard-mode
-          calendar-mode
-          process-menu-mode
-          helpful-mode
-          grep-mode
-          vterm-mode term-mode
-          magit-mode git-commit-mode
-          minibuffer-mode which-key-mode
-          org-agenda-mode org-capture-mode) . centaur-tabs-local-mode)
-
+  :hook ((after-init . centaur-tabs-mode)
+         ((dashboard-mode
+           calendar-mode
+           process-menu-mode
+           helpful-mode
+           grep-mode
+           vterm-mode term-mode
+           magit-mode git-commit-mode
+           minibuffer-mode which-key-mode
+           org-agenda-mode org-capture-mode) . centaur-tabs-local-mode))
   :bind (("C-<prior>" . centaur-tabs-backward)
          ("C-<next>" . centaur-tabs-forward)
          ("C-c t" . centaur-tabs-counsel-switch-group)
          (:map evil-normal-state-map
           ("g t" . centaur-tabs-forward)
           ("g T" . centaur-tabs-backward)))
-
   :init
   (setq centaur-tabs-style                    "chamfer"
         centaur-tabs-height                   32
@@ -60,11 +57,11 @@
   (centaur-tabs-group-by-projectile-project)
   (centaur-tabs-enable-buffer-reordering)
   (centaur-tabs-change-fonts (face-attribute 'default :font) centaur-tabs-font-size)
-  (centaur-tabs-mode t)
 
   :config
-  (defvar excluded-prefixes-append-list '("*flycheck-posframe-buffer*" "*Shell Command Output*"
-                                          "*Org Agenda*" "*dashboard*" "*Directory*" "*vterm*"))
+  (defvar excluded-prefixes-append-list
+    '("*flycheck-posframe-buffer*" "*Shell Command Output*"
+      "*Org Agenda*" " *" "*dashboard*" "*Directory*" "*vterm*"))
   (cl-loop for prefix in excluded-prefixes-append-list
            do (add-to-list 'centaur-tabs-excluded-prefixes prefix t)))
 
