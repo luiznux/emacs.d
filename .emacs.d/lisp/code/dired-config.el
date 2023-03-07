1c7c6f715b69337028861afc474bb74356d1e2a4
luiznux <luiztagli@hotmail.com>
2023-03-07 12:15:52 -0300

    Update dired.

--- .emacs.d/lisp/code/dired-config.el
+++ .emacs.d/lisp/code/dired-config.el
@@ -28,6 +28,9 @@
   :bind (:map dired-mode-map
          ("C-c C-p" . wdired-change-to-wdired-mode))
   :config
+  ;; Guess a default target directory
+  (setq dired-dwim-target t)
+
   ;; Always delete and copy recursively
   (setq dired-recursive-deletes 'always
         dired-recursive-copies 'always)
