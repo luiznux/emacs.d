1344d5ffcc3ae636ff93304b94c700643b633c52
luiznux <luiztagli@hotmail.com>
2023-08-14 19:47:12 -0300

    [Edit] integrate tree-sitter into expand-region.

--- lisp/code/init-edit.el
+++ lisp/code/init-edit.el
@@ -147,7 +147,23 @@
 
 ;; Increase selected region by semantic units
 (use-package expand-region
-  :bind ("C-=" . er/expand-region))
+  :bind ("C-=" . er/expand-region)
+  :config
+  (when (emacs-treesit-available-p)
+    (defun treesit-mark-bigger-node ()
+      "Use tree-sitter to mark regions."
+      (let* ((root (treesit-buffer-root-node))
+             (node (treesit-node-descendant-for-range root (region-beginning) (region-end)))
+             (node-start (treesit-node-start node))
+             (node-end (treesit-node-end node)))
+        ;; Node fits the region exactly. Try its parent node instead.
+        (when (and (= (region-beginning) node-start) (= (region-end) node-end))
+          (when-let ((node (treesit-node-parent node)))
+            (setq node-start (treesit-node-start node)
+                  node-end (treesit-node-end node))))
+        (set-mark node-end)
+        (goto-char node-start)))
+    (add-to-list 'er/try-expand-list 'treesit-mark-bigger-node)))
 
 ;; Smartly select region, rectangle, multi cursors
 (use-package smart-region
