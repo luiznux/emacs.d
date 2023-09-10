66935f664f2e1edd77e0ce021136132fa6d604d2
luiznux <luiztagli@hotmail.com>
2023-09-10 00:21:13 -0300

    [GO] add install tools

--- lisp/code/init-go.el
+++ lisp/code/init-go.el
@@ -17,14 +17,46 @@
 ;;; Code:
 
 (use-package go-mode
+  :functions (go-install-tools exec-path-from-shell-copy-envs)
   :autoload godoc-gogetdoc
   :bind (:map go-mode-map
          ("<f1>" . godoc))
-  :init (setq godoc-at-point-function #'godoc-gogetdoc)
+  :init
+  (setq godoc-at-point-function #'godoc-gogetdoc)
+  ;; Install tools
+  (defconst go--tools
+    '("golang.org/x/tools/gopls"
+      "golang.org/x/tools/cmd/goimports"
+      "honnef.co/go/tools/cmd/staticcheck"
+      "github.com/go-delve/delve/cmd/dlv"
+      "github.com/zmb3/gogetdoc"
+      "github.com/josharian/impl"
+      "github.com/cweill/gotests/..."
+      "github.com/fatih/gomodifytags"
+      "github.com/davidrjenni/reftools/cmd/fillstruct")
+    "All necessary go tools.")
+  (defun go-install-tools ()
+    "Install or update go tools."
+    (interactive)
+    (unless (executable-find "go")
+      (user-error "Unable to find `go' in `exec-path'!"))
+    (message "Installing go tools...")
+    (dolist (pkg go--tools)
+      (set-process-sentinel
+       (start-process "go-tools" "*Go Tools*" "go" "install" "-v" "-x" (concat pkg "@latest"))
+       (lambda (proc _)
+         (let ((status (process-exit-status proc)))
+           (if (= 0 status)
+               (message "Installed %s" pkg)
+             (message "Failed to install %s: %d" pkg status)))))))
   :config
   ;; Env vars
   (with-eval-after-load 'exec-path-from-shell
     (exec-path-from-shell-copy-envs '("GOPATH" "GO111MODULE" "GOPROXY")))
+
+  ;; Try to install go tools if `gopls' is not found
+  (unless (executable-find "gopls")
+    (go-install-tools))
 
   ;; Misc
   (use-package go-dlv)
