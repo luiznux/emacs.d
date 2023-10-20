11c30dbaf7a22b9d6973f6eeb2c21dae6cd022b7
luiznux <luiztagli@hotmail.com>
2023-10-20 15:43:42 -0300

    [popper] update rules

--- lisp/interface/init-window.el
+++ lisp/interface/init-window.el
@@ -90,7 +90,8 @@
           "\\*lsp-help\\*$" "\\*lsp session\\*$"
           "\\*quickrun\\*$"
           "\\*tldr\\*$"
-          "\\*vc-.*\\*$"
+          "\\*vc-.*\\**"
+          "\\*diff-hl\\**"
           "^\\*macro expansion\\**"
 
           "\\*Agenda Commands\\*" "\\*Org Select\\*" "\\*Capture\\*" "^CAPTURE-.*\\.org*"
