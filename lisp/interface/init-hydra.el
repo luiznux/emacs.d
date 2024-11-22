02ed68b7bfae6bfc3867d324514ea9fd2c0ef105
luiznux <luiztagli@hotmail.com>
2024-11-22 14:03:14 -0300

    [Hydra] fix invalid face in hydra title

--- lisp/interface/init-hydra.el
+++ lisp/interface/init-hydra.el
@@ -22,8 +22,6 @@
   :hook (emacs-lisp-mode . hydra-add-imenu))
 
 (use-package pretty-hydra
-  :custom (pretty-hydra-default-title-body-format-spec (format "%s %%s\n%%s"
-                                                               (propertize "\n" 'face '(:height 0.5))))
   :bind ("<f6>" . toggles-hydra/body)
   :hook (emacs-lisp-mode . (lambda ()
                              (add-to-list
@@ -35,7 +33,7 @@
   (cl-defun pretty-hydra-title (title &optional icon-type icon-name
                                       &key face height v-adjust)
     "Add an icon in the hydra title."
-    (let ((face (or face `(:inherit highlight :reverse-video t)))
+    (let ((face (or face 'mode-line-emphasis))
           (height (or height 1.2))
           (v-adjust (or v-adjust 0.0)))
       (concat
