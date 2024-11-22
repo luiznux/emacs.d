d23d3a76de055901cb95002ade24581f781c70fd
luiznux <luiztagli@hotmail.com>
2024-11-22 14:01:29 -0300

    [Vcs] Fix transient-posframe size

--- lisp/code/init-vcs.el
+++ lisp/code/init-vcs.el
@@ -62,8 +62,8 @@
   :hook (after-init . transient-posframe-mode)
   :init
   (setq transient-posframe-border-width 3
-        transient-posframe-min-height nil
         transient-posframe-min-width 80
+        transient-posframe-min-height nil
         transient-posframe-poshandler 'posframe-poshandler-frame-center
         transient-posframe-parameters '((left-fringe . 8)
                                         (right-fringe . 8)))
@@ -75,12 +75,12 @@
       (when (posframe-workable-p)
         (let* ((posframe
 	            (posframe-show buffer
-                               :height (with-current-buffer buffer (1- (count-screen-lines (point-min) (point-max))))
 			                   :font transient-posframe-font
 			                   :position (point)
 			                   :poshandler transient-posframe-poshandler
 			                   :background-color (face-attribute 'transient-posframe :background nil t)
 			                   :foreground-color (face-attribute 'transient-posframe :foreground nil t)
+                               :initialize #'transient-posframe--initialize
 			                   :min-width transient-posframe-min-width
 			                   :min-height transient-posframe-min-height
 			                   :internal-border-width transient-posframe-border-width
@@ -88,6 +88,20 @@
 			                   :override-parameters transient-posframe-parameters)))
           (frame-selected-window posframe))))
     (advice-add #'transient-posframe--show-buffer :override #'my-transient-posframe--show-buffer)
+
+    (setq transient-mode-line-format nil) ; without line
+
+    (defun transient-posframe--initialize ()
+      "Initialize transient posframe."
+      (setq window-resize-pixelwise t)
+      (setq window-size-fixed nil))
+
+    (defun transient-posframe--resize (window)
+      "Resize transient posframe."
+      (fit-frame-to-buffer-1 (window-frame window)
+                             nil transient-posframe-min-height
+                             nil transient-posframe-min-width))
+    (advice-add 'transient--fit-window-to-buffer :override #'transient-posframe--resize)
 
     (defun my-transient-posframe--hide ()
       "Hide transient posframe."
