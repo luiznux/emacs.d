9b1f45c20ca6d494d25276900f77dc1536b8b668
luiznux <luiztagli@hotmail.com>
2025-04-15 01:26:41 -0300

    [Python] Update config add new pyright server

--- lisp/lang/init-python.el
+++ lisp/lang/init-python.el
@@ -26,7 +26,8 @@
                                    (get-process "Python"))))
   :init
   ;; Disable readline based native completion
-  (setq python-shell-completion-native-enable nil)
+  (setq python-shell-completion-native-enable nil
+        python-indent-guess-indent-offset-verbose nil)
   :config
   ;; Default to Python 3. Prefer the versioned Python binaries since some
   ;; systems stupidly make the unversioned one point at Python 2.
@@ -38,45 +39,63 @@
   (with-eval-after-load 'exec-path-from-shell
     (exec-path-from-shell-copy-env "PYTHONPATH"))
 
-  ;; With basedpyright
-  ;; See https://docs.basedpyright.com/latest/
-  (use-package lsp-pyright
-    :init
-    (when (executable-find "python3")
-      (setq lsp-pyright-python-executable-cmd "python3"))
+  (add-hook! 'python-mode-hook
+    (defun +python-use-correct-flycheck-executables-h ()
+      "Use the correct Python executables for Flycheck."
+      (let ((executable python-shell-interpreter))
+        (save-excursion
+          (goto-char (point-min))
+          (save-match-data
+            (when (or (looking-at "#!/usr/bin/env \\(python[^ \n]+\\)")
+                      (looking-at "#!\\([^ \n]+/python[^ \n]+\\)"))
+              (setq executable (substring-no-properties (match-string 1))))))
+        ;; Try to compile using the appropriate version of Python for
+        ;; the file.
+        (setq-local flycheck-python-pycompile-executable executable)
+        ;; We might be running inside a virtualenv, in which case the
+        ;; modules won't be available. But calling the executables
+        ;; directly will work.
+        (setq-local flycheck-python-pylint-executable "pylint")
+        (setq-local flycheck-python-flake8-executable "flake8")))))
 
-    (when (executable-find "basedpyright")
-      (setq lsp-pyright-langserver-command "basedpyright")))
+(use-package python-black
+  :hook ((python-mode python-ts-mode) . python-black-on-save-mode))
 
-  (use-package python-black
-    :hook ((python-mode python-ts-mode) . python-black-on-save-mode))
+(use-package pyimport
+  :commands +python/optimize-imports
+  :init
+  (defun +python/optimize-imports ()
+    "organize imports"
+    (interactive)
+    (pyimport-remove-unused)
+    (py-isort-bufer))
 
-  (use-package pyimport
-    :commands +python/optimize-imports
-    :init
-    (defun +python/optimize-imports ()
-      "organize imports"
-      (interactive)
-      (pyimport-remove-unused)
-      (py-isort-bufer))
+  (map! :after python
+        :map python-mode-map
+        :localleader
+        :prefix ("i" . "imports")
+        :desc "Insert missing imports" "i" #'pyimport-insert-missing
+        :desc "Remove unused imports"  "R" #'pyimport-remove-unused
+        :desc "Optimize imports"       "o" #'+python/optimize-imports))
 
-    (map! :map python-mode-map
-          :localleader
-          :prefix ("i" . "imports")
-          :desc "Insert missing imports" "i" #'pyimport-insert-missing
-          :desc "Remove unused imports"  "R" #'pyimport-remove-unused
-          :desc "Optimize imports"       "o" #'+python/optimize-imports))
+(use-package py-isort
+  :init
+  (map! :map python-mode-map
+        :localleader
+        (:prefix ("i" . "imports")
+         :desc "Sort imports"      "s" #'py-isort-buffer
+         :desc "Sort region"       "r" #'py-isort-region)))
 
-  (use-package py-isort
-    :init
-    (map! :map python-mode-map
-          :localleader
-          (:prefix ("i" . "imports")
-           :desc "Sort imports"      "s" #'py-isort-buffer
-           :desc "Sort region"       "r" #'py-isort-region)))
+;; Live Coding in Python
+(use-package live-py-mode)
 
-  ;; Live Coding in Python
-  (use-package live-py-mode))
+;; LSP
+;; With basedpyright
+;; See https://docs.basedpyright.com/latest/
+(use-package lsp-pyright
+  :init
+  (when (executable-find "basedpyright")
+    (setq lsp-pyright-langserver-command "basedpyright")))
 
 
 (provide 'init-python)
