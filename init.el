6fcb1b15c6cd072713171f7145f182d55fbdfd86
luiznux <luiztagli@hotmail.com>
2025-07-07 13:56:05 -0300

    [Init] refactor init.el

--- init.el
+++ init.el
@@ -20,47 +20,55 @@
   (error "This requires Emacs 29.1 and above!"))
 
 ;;
-;; Speed up startup
+;; Speed up startup Process
 ;;
 
-;; Defer garbage collection further back in the startup process
+;; Optimize Garbage Collection for Startup
 (setq gc-cons-threshold most-positive-fixnum)
 
 ;; Prevent flash of unstyled modeline at startup
 (setq-default mode-line-format nil)
 
-;; Don't pass case-insensitive to `auto-mode-alist'
+;; Optimize `auto-mode-alist'
 (setq auto-mode-case-fold nil)
 
 (unless (or (daemonp) noninteractive init-file-debug)
-  ;; Suppress file handlers operations at startup
+  ;; Temporarily suppress file-handler processing to speed up startup
   ;; `file-name-handler-alist' is consulted on each call to `require' and `load'
-  (let ((old-value file-name-handler-alist))
+  (let ((default-handlers file-name-handler-alist))
     (setq file-name-handler-alist nil)
-    (set-default-toplevel-value 'file-name-handler-alist file-name-handler-alist)
-    (put 'file-name-handler-alist 'initial-value old-value)
+    ;; Recover handlers after startup
     (add-hook 'emacs-startup-hook
               (lambda ()
                 "Recover file name handlers."
                 (setq file-name-handler-alist
-                      (delete-dups (append file-name-handler-alist old-value))))
+                      (delete-dups (append file-name-handler-alist default-handlers))))
               101)))
 
-;; Load path
-;; Optimize: Force "lisp" at the head to reduce the startup time.
+;;
+;; Configure Load Path
+;;
+
+;; Add "lisp" and "site-lisp" to the beginning of `load-path`
 (defun update-load-path (&rest _)
-  "Update `load-path'."
+  "Update the `load-path' to prioritize personal configurations."
   (dolist (dir '("lisp" "site-lisp"))
     (push (expand-file-name dir user-emacs-directory) load-path)))
 
+;; Add subdirectories inside "site-lisp" and "lisp" to `load-path'
 (defun add-subdirs-to-load-path (&rest _)
-  "Add subdirectories to `load-path'."
+  "Recursively add subdirectories in `site-lisp` to `load-path`.
+
+Avoid placing large files like EAF in `site-lisp` to prevent slow startup."
   (dolist (dir '("lisp" "site-lisp"))
     (let ((default-directory (expand-file-name dir user-emacs-directory)))
       (normal-top-level-add-subdirs-to-load-path))))
 
+;; Ensure these functions are called after `package-initialize
 (advice-add #'package-initialize :after #'update-load-path)
 (advice-add #'package-initialize :after #'add-subdirs-to-load-path)
+
+;; Initialize load paths explicitly
 (update-load-path)
 
 ;; Requisites
