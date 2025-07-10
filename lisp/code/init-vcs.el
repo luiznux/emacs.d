2fd718fa9def11b093d9378c4c0832db324b824e
luiznux <luiztagli@hotmail.com>
2025-07-10 20:17:09 -0300

    [Vcs] refactor forge and magit-todos

--- lisp/code/init-vcs.el
+++ lisp/code/init-vcs.el
@@ -28,31 +28,17 @@
   ;; Access Git forges from Magit
   (use-package forge
     :demand t
-    :custom-face
-    (forge-topic-label ((t (:inherit variable-pitch :height 0.9 :width condensed :weight regular :underline nil))))
     :init
-    (setq forge-topic-list-columns
-          '(("#" 5 forge-topic-list-sort-by-number (:right-align t) number nil)
-            ("Title" 60 t nil title  nil)
-            ("State" 6 t nil state nil)
-            ("Updated" 10 t nil updated nil)))
-
     ;; avoid conflics with `evil-collection-forge-setup'
     (setq forge-add-default-bindings nil))
 
   ;; Show TODOs in magit
   (use-package magit-todos
-    :defines magit-todos-nice
-    :commands magit-todos-mode magit-todos--scan-with-git-grep
+    :after magit-status
+    :commands magit-todos-mode
     :init
     (setq magit-todos-nice (if (executable-find "nice") t nil))
-    (setq magit-todos-scanner #'magit-todos--scan-with-git-grep)
-    (let ((inhibit-message t))
-      (magit-todos-mode 1))
-    :config
-    (with-eval-after-load 'magit-status
-      (transient-append-suffix 'magit-status-jump '(0 0 -1)
-        '("t " "Todos" magit-todos-jump-to-todos)))))
+    (magit-todos-mode 1)))
 
 (use-package transient-posframe
   :diminish
